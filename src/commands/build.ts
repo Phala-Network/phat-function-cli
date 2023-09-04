e32505c7ea4cb070e1b79663545d465eeb20b425
pacoyang <yangbaigao@gmail.com>
2023-09-04 20:45:05 +0800

    feat: specify the file to build

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,6 +1,6 @@
 import { lstatSync, readFileSync, statSync } from 'node:fs'
 import path from 'node:path'
-import { Command, Flags, ux } from '@oclif/core'
+import { Args, Command, Flags, ux } from '@oclif/core'
 import webpack, { Configuration, Stats } from 'webpack'
 import VirtualModulesPlugin from 'webpack-virtual-modules'
 import { merge, mergeWithCustomize, customizeArray } from 'webpack-merge'
@@ -190,7 +190,15 @@ function printFileSizesAfterBuild(
 }
 
 export default class Build extends Command {
-  static description = 'Build a production bundle of your JS'
+  static description = 'Build a production bundle of the function script'
+
+  static args = {
+    script: Args.string({
+      description: 'The function script file',
+      require: true,
+      default: 'src/index',
+    }),
+  }
 
   static flags = {
     location: Flags.string({
@@ -212,7 +220,7 @@ export default class Build extends Command {
   }
 
   public async run(): Promise<void> {
-    const { flags } = await this.parse(Build)
+    const { flags, args: { script } } = await this.parse(Build)
     const directory = flags.location ? resolveToAbsolutePath(flags.location) : process.cwd()
     const isDev = flags.mode === 'development' || flags.mode === 'dev'
     if (!lstatSync(directory).isDirectory()) {
@@ -221,7 +229,7 @@ export default class Build extends Command {
 
     const outputDir = path.resolve(directory, flags.output ?? 'dist')
     let buildEntries: Record<string, string> = {
-      index: 'src/index',
+      index: script,
     }
     const pjson = JSON.parse(readFileSync(path.join(directory, 'package.json')).toString())
     if (pjson.exports && typeof pjson.exports !== 'string') {
