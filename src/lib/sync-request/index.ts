71d9b08e74188a883015bc3ce0137b50b821c0cc
pacoyang <yangbaigao@gmail.com>
2024-02-07 12:10:44 +0800

    fix: preventing path errors on windows

--- src/lib/sync-request/index.ts
+++ src/lib/sync-request/index.ts
@@ -1,9 +1,10 @@
+import upath from 'upath'
 import type { Dispatcher } from 'undici'
 
 export type HttpMethod = Dispatcher.HttpMethod
 
 const rpc = require('sync-rpc')
-const remote = rpc(require.resolve('./worker'))
+const remote = rpc(require.resolve(upath.join(__dirname, 'worker')))
 
 export default function (
   options: Omit<Dispatcher.RequestOptions, 'path'> & {
